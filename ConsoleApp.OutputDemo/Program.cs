d9e2a888bfa5b42768d64c82839676e4d34d258b
pgrah <pgrah@LAPTOP-AQJP9HJR>
2025-04-08 00:51:31 -0500

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Writeline is a method that takes a block of text and print it to the screen
+Console.WriteLine("Hello, World!My name is Jodi ann Charlton");
+
+Console.WriteLine("I am from Jamaica!");
+
+Console.WriteLine(" Anything you want to say");
+
+//Write is a method that takes a block of text and print it to the screen.
+//it does not break the line after it print the text.
+Console.Write("Line 1");
+Console.Write("line 2");
---
+++ ConsoleApp.variablesDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// different dadttypes
+/*
+    text - string
+    intergers - int
+    decimal - double , float , decimal
+    logical - bool 
+*/
+string name = "Jodi ann Charlton";
+
+Console.WriteLine(name);
+Console.WriteLine("I am "+ name);   // String concatenation text is call static message string
+Console.WriteLine($"They call me {name}");  // String interpolation
+Console.WriteLine("I was given the name {0}", name);  // Formatted string
+
+int age = 54;
+int retirementYearsLeft = 11;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("My age is: " + age);
+Console.WriteLine("My retirement age is: " + retirementAge);
+
+
+bool isRetired = false;
+Console.WriteLine("am i retired? " + isRetired);
+
+
+
